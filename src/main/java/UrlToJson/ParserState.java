7fe415ac70777ab7f74db867ca34d420a3944912
Victor Yevitchenko <cheer4engineer@gmail.com>
2017-02-08 12:47:00 +0500

    init commit

---
+++ src/main/java/UrlToJson/ParserState.java
@@ -0,0 +1,42 @@
+package UrlToJson;
+
+
+import com.fasterxml.jackson.core.JsonGenerator;
+
+import java.io.IOException;
+
+public enum ParserState {
+    InObject {
+        @Override void open(JsonGenerator generator, String objectName) throws IOException {
+            generator.writeObjectFieldStart(objectName);
+        }
+
+        @Override void close(JsonGenerator generator) throws IOException {
+            generator.writeEndObject();
+        }
+    }, InArray {
+        @Override
+        void open(JsonGenerator generator, String arrayName) throws IOException {
+            generator.writeFieldName(arrayName);
+            generator.writeStartArray();
+
+        }
+
+        @Override void close(JsonGenerator generator) throws IOException {
+            generator.writeEndArray();
+        }
+
+    }, InArrayElement {
+        @Override void open(JsonGenerator generator, String objectName) throws IOException {
+            generator.writeStartObject();
+        }
+
+        @Override void close(JsonGenerator generator) throws IOException {
+            generator.writeEndObject();
+        }
+    };
+
+    abstract void open(JsonGenerator generator, String fieldName) throws IOException;
+
+    abstract void close(JsonGenerator generator) throws IOException;
+}
